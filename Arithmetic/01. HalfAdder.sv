36c3a1e16df6c9e249e77c06059f0c548f6a23e4
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-04-07 17:59:05 -0700

    Update 01. HalfAdder.sv

--- Arithmetic/01. HalfAdder.sv
+++ Arithmetic/01. HalfAdder.sv
@@ -1,33 +1,3 @@
-/*Arithmetic Circuit
-Combinational Circuit - Not sequential because it doesnt have memory element
-
-Application - Arithmetic Units
-1. Basic building block for: Full Adders, Ripple Carry Adders, Carry Lookahead Adders
-2. ALUs (Arithmetic Logic Units)
-Inside CPUs, GPUs, DSPs (as part of larger adders)
-3. Multipliers Partial product addition stages
-4. Address Generation Increment logic (small add operations)
-
-RTL Applications 
-Where it is used:
-Educational designs
-Small custom datapaths
-Bit-level arithmetic optimizations
-Low-power or highly optimized custom logic
-1. Basic DUT (Device Under Test)
-Used in:
-Testbench learning
-UVM tutorials
-Sanity checking environments
-2. Assertion Writing Practice
-assert property (@(posedge clk) Sum == (A ^ B));
-3. Functional Coverage
-covergroup cg;
-    coverpoint {A, B};
-endgroup
-4. Golden Reference Models
-Used as simple reference logic in scoreboards*/
-
 //--------------------------- Design ---------------------------//
 
 module adder(input logic a, b,
