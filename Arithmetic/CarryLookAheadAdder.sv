e95b70e6783829cc53ba2a5dda8fad8724dfcc69
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-04-17 17:36:07 -0700

    Update CarryLookAheadAdder.sv

--- Arithmetic/CarryLookAheadAdder.sv
+++ Arithmetic/CarryLookAheadAdder.sv
@@ -6,22 +6,23 @@ module carryLookAhead #(parameter WIDTH=4)
   
   logic [WIDTH-1:0] g, p;
   logic [WIDTH:0]c;
-  assign c[0] = 0;
+  assign c[0] = cin;
     
   	genvar i;
   		generate 
           for(i=0; i<WIDTH; i++) begin
-            assign g[i] = a&b;
-            assign p[i] = (a^b);
+            assign g[i] = a[i] & b[i];
+            assign p[i] = a[i] ^ b[i];
           end
         endgenerate
   
-  assign c[1] = g[1] | (p[1]&c[0]);
-  assign c[2] = g[1] | (p[1]&g[0]) | (p[1]&p[0]&c[0]);
-  assign c[3] = g[2] | (p[2]&g[1]) | (p[2]&p[1]&g[0]) | (p[2]&p[1]&p[0]&c[0]);
-  assign c[4] = g[3] | (p[3]&g[2]) | (p[3]&p[2]&g[1]) | (p[3]&p[2]&p[1]&p[0]&c[0]);
-  
   generate
+    for(i=0; i<WIDTH; i++) begin
+      assign c[i+1] = g[i] | (p[i] & c[i]);
+    end
+  endgenerate 
+  
+  generate 
     for(i=0; i<WIDTH; i++) begin
       assign sum[i] = p[i] ^ c[i];
     end
@@ -31,28 +32,35 @@ module carryLookAhead #(parameter WIDTH=4)
 endmodule 
 
 //TB
-module carryLookAhead_tb #(parameter WIDTH=4) ;
-  
+module carryLookAhead_tb #(parameter WIDTH=4);
+
   logic [WIDTH-1:0] a, b;
   logic cin;
   logic [WIDTH-1:0] sum;
   logic cout;
-  logic expected = 0;
-  
+  logic [WIDTH:0] expected;
+
   carryLookAhead dut(.*);
-  
+
   initial begin
     $dumpfile("dumps.vcd");
-    $dumpvars(1);
-    
+    $dumpvars(0, carryLookAhead_tb);
+
     repeat (512) begin
-      a = $random();
-      b = $random();
-      cin = $random();
-      
+      a = $random() % (1 << WIDTH);
+      b = $random() % (1 << WIDTH);
+      cin = $random() % 2;
+
       #1;
-      $display ("a=%0d b=%0d cin=%0d | sum=%0d cout=%0d ", a, b, cin, sum, cout);
-      #1;
+      expected = a + b + cin;
+
+      if ({cout, sum} !== expected) begin
+        $display("ERROR: a=%0d b=%0d cin=%0d | expected=%0d actual=%0d", a, b, cin, expected, {cout, sum});
+      end
+      else begin
+        $display("PASS!: a=%0d b=%0d cin=%0d | expected=%0d actual=%0d", a, b, cin, expected, {cout, sum});
     end
+    $display("Test completed");
   end
-endmodule 
+  end
+endmodule
