0126c58e3229632429f39480365834b0c98685f4
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-06-23 22:49:58 -0700

    Refactor LFSR module for improved functionality

--- Practice/LFSR.sv
+++ Practice/LFSR.sv
@@ -1,23 +1,47 @@
 //Design 
 module LFSR(
-  input logic clk, rstn, 
+  input logic clk, rst, 
+  input logic mode,
   output logic [7:0] q);
   
-  logic feedback;
-  assign feedback = q[7] ^ q[0];
+  logic [7:0] q_next;
+  logic [7:0] q_f, q_g;
   
-  always_ff @(posedge clk or negedge rstn) begin
-    if(!rstn) 
+  
+  always_ff @(posedge clk, posedge rst) begin
+    if(rst) 
       q <= 8'b00000001;
     else
-      q <= {q[6:0], feedback};
+      q <= q_next;
   end
+  
+  always_comb begin
+    logic feedback;
+  	feedback = (q[7] ^ q[0]) ^ (q[5] ^ q[6]);
+    
+    q_f = {q[6:0], feedback};
+  end
+  
+  always_comb begin
+      logic fb;
+    logic [7:0] shifted;
+    
+      shifted = q >> 1;
+      
+    fb = q[7];
+    if(fb == 1)
+      shifted ^= {q[0], q[5], q[6], q[7]};
+
+  end
+  
+  assign q_next = (mode) ? q_f: q_g;
 endmodule 
 
 //Testbench
 module LFSR_tb;
   
   logic clk, rstn;
+  logic mode;
   logic [7:0] q;
   
   LFSR dut (.*);
@@ -30,7 +54,8 @@ module LFSR_tb;
     
     #12;
     rstn=1;
-    
+    #5;
+    mode = 0;
     repeat(20) begin
       @ (posedge clk); 
       
@@ -42,6 +67,25 @@ module LFSR_tb;
         q != 8'b00000000
       );
     end
+        
+        #10;
+        mode = 1;
+        repeat(20) begin
+      @ (posedge clk); 
+      
+      $display("[%0t] q=%08b", $time, q);
+      
+      assert property (
+        @(posedge clk) 
+        disable iff(!rstn)
+        q != 8'b00000000
+      );
+    end
+        
     $finish;
   end
 endmodule 
+        
+        
+        
+        
