46a261fd189ab142d0f3c5c51267e30a5366001d
Amogh Vinod Thakur <90680979+AmoghT23@users.noreply.github.com>
2026-06-25 13:04:18 -0700

    Add counter for tick and improve reset handling
    Refactor LFSR module to include a counter for tick generation and update reset logic.

--- Practice/LFSR.sv
+++ Practice/LFSR.sv
@@ -1,17 +1,29 @@
 //Design 
-module LFSR(
+module lfsr_fpga(
   input logic clk, rst, 
   input logic mode,
   output logic [7:0] q);
   
   logic [7:0] q_next;
   logic [7:0] q_f, q_g;
+  reg [24:0] counter;
+  reg tick;
   
+  always_ff @(posedge clk, negedge rst) begin
+	if(!rst) 
+		counter <= '0;
+	else if (counter == 25000000)
+		counter <= '0;
+	else 
+		counter <= counter + 1'b1;
+	end
+	
+  assign tick = (counter == 25000000);
   
-  always_ff @(posedge clk, posedge rst) begin
-    if(rst) 
+  always_ff @(posedge clk, negedge rst) begin
+    if(!rst) 
       q <= 8'b00000001;
-    else
+    else if(tick) 
       q <= q_next;
   end
   
@@ -23,15 +35,11 @@ module LFSR(
   end
   
   always_comb begin
-      logic fb;
     logic [7:0] shifted;
     
-      shifted = q >> 1;
+    shifted = {q[6:0], 1'b0};
       
-    fb = q[7];
-    if(fb == 1)
-      shifted ^= {q[0], q[5], q[6], q[7]};
-
+    q_g = q[7] ? (shifted ^ 8'b00000111) : shifted;
   end
   
   assign q_next = (mode) ? q_f: q_g;
